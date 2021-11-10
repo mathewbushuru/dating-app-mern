e7248b1f753290ca924e958f353a8c259a5e3e75
Mathew Bushuru <bushuruwafulamathew@gmail.com>
2021-11-09 22:27:12 -0800

    Added header component with icon for profile and messages

--- dating-app-frontend/src/App.js
+++ dating-app-frontend/src/App.js
@@ -1,11 +1,11 @@
+import './App.css';
 
-
-import './App.css'
+import Header from './components/Header';
 
 function App(){
   return (
     <div className="app">
-      <h1>Dating App using MERN</h1>
+      <Header/>
     </div>
   )
 }
